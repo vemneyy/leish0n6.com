e70bacb985b0b077ddca0e36a0f1b520cb228dec
leish0n6 <78843201+leish0n6@users.noreply.github.com>
2021-05-04 22:02:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+   
+    $url = array(
+        'https://leish0n6.com/cyberpunk',
+        'https://leish0n6.com/desert',
+        'https://leish0n6.com/forest',
+        'https://leish0n6.com/newyork',
+        'https://leish0n6.com/losangeles',
+        'https://leish0n6.com/santamonica'
+    );
+    
+   
+    $key = array_rand( $url );
+    
+   
+    header( 'Location: ' . $url[$key] );
+?>
